6ad28d9a46e863253137b1c565e519c2459f5944
akkayaasli <akkayaasli19@gmail.com>
2021-05-06 12:23:05 +0300

    Update Liste.js

--- src/components/Liste.js
+++ src/components/Liste.js
@@ -3,29 +3,19 @@ import { ScrollView} from 'react-native';
 
 import axios from 'axios';
 import Detay from './Detay';
-
-
-
 //2: <View style={{marginTop:5}}> yazma sebebi önceden verdiğimiz shadow yeni eklenen view den dolayı görünmüyordu. 5 cm kaydırdık.
 
 class Liste extends Component{
-
     state = {data: [] };
-
     componentDidMount(){//internetten veri alıcaz.
        // console.log('componentDidMount'); çalısıp calısmadıgı terminalden kontrol edildi.
 
        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
        .then(response=>this.setState({data:response.data}));//çektiğim datayı state içindeki data array ne aktarıyorum.
-
-
        //yukarda yazılan adres response deniyor.
        //Biz bu responsa içindeki dataları alıp dizi halinde göstermeye çalışıyoruz.
-
     }
-
     renderData(){//bu metot ile belirtile url deki öncelikle title kısımlarını alıcaz.
-
         return this.state.data.map(responseData=>
             /*<Text>
                 {responseData.title}
@@ -33,12 +23,9 @@ class Liste extends Component{
 
             <Detay data={responseData}/>//ıd adında bir id ataması yapıldı.Burada data değikenine atadığımız bilgiyi Detay.js de yazıcaz.
             );
-
     }
-
     render()
     {
-
         /*console.log('gelen_data'+this.state);*/   //state içi boş geliyormu kontrolü terminalden sağlanıyor.
         //gelen_data[object Object] terminalde böyle geldi.Demek ki içi dolu geliyor.
 
@@ -56,27 +43,6 @@ class Liste extends Component{
     };
     
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 /*const Liste = () =>{
     return(
         <View style={{marginTop:5}}>
